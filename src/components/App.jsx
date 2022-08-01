be5d6aa335f13c4cc8fb299c9e07b44c1ad75637
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-01 14:36:41 +0300

    react початок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,25 @@ import { fetchEvents } from '../service/service';
 import { EventGallary } from '../components/Eventgallary/Eventgallary';
 import { eventsById } from '../service/service';
 import { Details } from '../components/Details/Details';
+// import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
+  //   <Routes>
+  //     <Route path="/" element={<Layout />}>
+  //       <Route index element={<Home />} />
+  //       <Route path="/events" element={<Events />} />
+  //     </Route>
+  //   </Routes>;
+  // };
+
   const [eventsList, setEventsList] = useState([]);
   const [event, setEvent] = useState(null);
-
   useEffect(() => {
     if (eventsList.length === 0) fetchEvents().then(setEventsList);
   }, [eventsList]);
-
   const handleFetchEvents = id => {
     eventsById(id).then(setEvent);
   };
-
   return (
     <>
       <EventGallary events={eventsList} fetchEvents={handleFetchEvents} />
